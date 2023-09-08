77f4d99df8297c43378a26bea549f12660beae56
Lasse Haverinen <lasse.haverinen@gmail.com>
2023-09-08 11:42:26 +0300

    Friday 8th Sept Exercise lesson updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
-import Exercise3_5 from "./exercises-3-5/Exercise3_5";
-import Exercise4_1 from "./exercises-4-1/Exercise4_1";
+import Exercise3_5 from "./exercise-3-5/Exercise3_5";
+import Exercise4_1 from "./exercise-4-1/Exercise4_1";
+import Exercise4_3 from "./exercise-4-3/Exercise4_3";
+import Exercise4_4 from "./exercise-4-4/Exercise4_4";
+import Exercise4_5 from "./exercise-4-5/Exercise4_5";
+import Exercise5_1 from "./exercise-5-1/Exercise5_1";
 
 function App() {
   return (
     <div className="App">
       <Exercise3_5 />
       <Exercise4_1 />
+      <Exercise4_3 />
+      <Exercise4_4 />
+      <Exercise4_5 />
+      <Exercise5_1 />
     </div>
   );
 }
